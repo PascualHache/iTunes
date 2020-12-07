2ad9e8c9bc62226924b8f3bb929eb5ec4fc644c6
Hache <hector.pascual.diaz@gmail.com>
2020-12-07 19:46:06 +0100

    AppBar implemented

--- itunes-slashmobility/src/App.js
+++ itunes-slashmobility/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './App.css';
-import {fetchSongs} from './actions/action';
+// import { fetchSongs } from './actions/action';
+import SongCard from './components/card';
+import MenuBar from './components/menuBar2';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <div>
-        <button onClick={() => this.props.dispatch(fetchSongs())}>Show SONGS</button>
+
+    return (<div>
+      <MenuBar params={this.props} />
+      {/* <button onClick={() => this.props.dispatch(fetchSongs("Methodman"))}>Show SONGS</button> */}
+      <div className="generalGrid">
         {this.props.loading
           ? <p>Loading...</p>
           : this.props.error
             ? <p>Error, try again</p>
             : this.props.results && this.props.results.map(item => {
-              return <div key={item.trackId}><img alt={item.trackId} src={item.artworkUrl100} />
-                <h2>{item.trackName}</h2>
-                <h3>{item.collectionName}</h3>
+              return <div key={item.trackId} className="cardGrid">
+                <SongCard
+                  img={item.artworkUrl100}
+                  title={item.trackName}
+                  album={item.collectionName}
+                  className="cardSingle" />
               </div>
             })}
       </div>
+    </div>
     )
   }
 }
